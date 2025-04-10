735e8dfb38587fa0671257b61b9d839a16123f76
mind <mind@summits-Mac-mini.local>
2025-04-10 19:12:02 +0530

    fix

--- src/modules/events/utils/events.constant.js
+++ src/modules/events/utils/events.constant.js
@@ -1,8 +1,6 @@
 module.exports = {
-  CATEGORY: {
-    USER: 'User',
-    EVENT: 'Event',
-    SAVED_EVENT: 'Saved Event',
-    TRIAL_EVENT: 'Trial Event',
-  },
+  USER: 'User',
+  EVENT: 'Event',
+  SAVED_EVENT: 'Saved Event',
+  TRIAL_EVENT: 'Trial Event',
 };
