c38fcbcbde18de4b70a0ef851e2eaf93672e542f
mind <mind@summits-Mac-mini.local>
2025-04-23 15:46:31 +0530

    fix

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ app.use(
   '/event-management/payment/webhooks',
   express.raw({ type: 'application/json' })
 );
-// app.use(
-//   '/event-management/subscription/webhooks',
-//   express.raw({ type: 'application/json' })
-// );
+app.use('/event-management/subscription/webhooks', express.json());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
